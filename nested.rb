db9c7c358fe026fc05693fedd5f9aa0541fa4888
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2019-06-19 01:45:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,7 +128,7 @@ def adding_to_dennis
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages][2] = "Assembly"
+     programmer_hash[:dennis_ritchie][:languages]= "Assembly"
      
      return programmer_hash
 
